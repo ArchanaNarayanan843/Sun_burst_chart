2fd8d97eafbe2f8df91aae61f55dd929bdc9b248
Archana Narayanan <75875071+ArchanaNarayanan843@users.noreply.github.com>
2022-11-21 19:55:58 +0530

    Add files via upload
    commit

---
+++ file.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pickle
+
+df_sun=pickle.load(open('sun_df.pkl','rb'))
+sun=pd.DataFrame(df_sun)
+
+st.title('Sunburst chart')
+
+fig=px.sunburst(sun,
+               path=['watch','Brand','Product_name'],
+               values='Count',
+               title='Top Smart watch brands',
+               width=750, height=750)
+
+
+fig.show()
