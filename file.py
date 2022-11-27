f59d47552b5d6ac0dd891eb4b7cd25c471da7b31
Archana Narayanan <75875071+ArchanaNarayanan843@users.noreply.github.com>
2022-11-27 12:21:30 +0530

    Update file.py
    fgfg

--- file.py
+++ file.py
@@ -1,20 +1,25 @@
+import seaborn as sns
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import plotly.express as px
+import plotly.figure_factory as ff
 import pickle
 
 df_sun=pickle.load(open('sun_df.pkl','rb'))
 sun=pd.DataFrame(df_sun)
 
+df=pickle.load(open('datahut_df.pkl','rb'))
+df=pd.DataFrame(df)
+
 st.title('Sunburst chart')
 
-fig=px.sunburst(sun,
-               path=['watch','Brand','Product_name'],
-               values='Count',
-               title='Top Smart watch brands',
-               width=750, height=750)
 
-
-fig.show()
+fig = px.sunburst(sun,
+             path=['watch','Brand','Product_name'],
+             values='Count',
+             title='Top Smart watch brands',
+             width=700, height=700)
+# fig.show()
+st.plotly_chart(fig)
